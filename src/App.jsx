8bf77d5e3dd064956c70ed24a5e97fd50918d5cd
VictorLucchi <rm95494@estudante.fieb.edu.br>
2025-09-18 07:41:26 -0300

    Iniciamos denovo

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,11 @@
-import {useState} from "react";
-import poke from './assets/pokeapi_256.png'
+const App = () => {
 
+  return(
+    <>
+    <h1>Projeto Pizzaria</h1>
+    </>
+  )
 
-function App(){
-
-  
-
- // var nome = "Anjo";
-
- const Formulario = (props) => {
- 
-  const [nome, setnome] = useState('pandinha')
-
-    return (
-        <div>
-           <input
-          className="nome"
-          name="nome"
-          onChange={(e)=>{setnome(e.target.value)}}
-          placeholder={props.sombra != null ? props.sombra : "texto padrão"}
-        type="text" />
-        <button 
-        className="butaum"
-        onClick={()=>{alert(nome)}}>
-        CLIQUE AQUI
-
-        </button>
-
-        </div>
-
-    )
-
- }
-
-return(
-      <div>
-        <h3>Pizzaria 2f</h3>
-        <p>texto do paragrafo</p>
-        <img src={poke} style={{width:200, height:100 }}/>
-       <Formulario sombra="digite seu nome..."/>
-       <Formulario sombra="digite seu e-mail..."/>
-       <Formulario sombra="(99) 90000-0000"/>
-       <Formulario/>
-      </div>
-
-
-)
 }
 
 export default App
