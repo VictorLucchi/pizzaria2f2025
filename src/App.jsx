b7e9e4dfcb8e565a5fda65d5623706d0e5886d2d
VictorLucchi <rm95494@estudante.fieb.edu.br>
2025-09-19 07:47:45 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import Produtos from "./pages/produtos"
+
 const App = () => {
 
   return(
     <>
     <h1>Projeto Pizzaria</h1>
+    <Produtos />
+    <Produtos />
+    <Produtos />
+    <Produtos />
+    <Produtos />
+    <Produtos />
+    <Produtos />
     </>
   )
 
---
+++ src/pages/produtos/index.jsx
@@ -0,0 +1,16 @@
+const Produtos = () => {
+
+    return (
+        <div>
+            <ul>
+                <li>Pizza de Muçarela</li>
+                <li>Pizza de Calabresa</li>
+                <li>pizza Portuguesa</li>
+                <li>Pizza Baiana</li>
+            </ul>
+        </div>
+    )
+
+}
+
+export default Produtos
